cec30b33b4b67339429f19d84e19ddc99b61d38d
TonyTangWJ <MC46509@connect.um.edu.mo>
2025-09-18 22:30:09 +0800

    start importance test

--- data_model/rolling_train_test.py
+++ data_model/rolling_train_test.py
@@ -53,7 +53,7 @@ class RollingTrainTest:
             print (f'No.{_+1} Test Predictability: {result:.4f}')
             print (f'No.{_+1} Train Predictability: {result_train:.4f}')
             # save test predictability and train predictability
-            file = f'../CSV_final/profit/predictability_details/{self.model_name}.csv'
+            file = f'../CSV_final/importance/predictability_details/{self.model_name}.csv'
             mode = 'a' if os.path.exists(file) else 'w'
             with open(file, mode) as f:
                 if mode == 'w':
@@ -67,7 +67,7 @@ class RollingTrainTest:
         self.act = np.concatenate(self.act_list, axis=0)
         all_spent_time = round(time.time() - all_start_time, 2)
         print(f"Predictability of {self.model_name}: {sum(self.predictability) / len(self.predictability):.4f}")
-        file = '../CSV_final/predictability.csv'
+        file = '../CSV_final/importance/predictability.csv'
         mode = 'a' if os.path.exists(file) else 'w'
         with open(file, mode) as f:
             if mode == 'w':
@@ -114,7 +114,7 @@ class RollingTrainTest:
                 'cum_returns': cum_returns}
             )
 
-            file = f'../CSV_final/profit/profit_details/{self.model_name}.csv'
+            file = f'../CSV_final/importance/profit_details/{self.model_name}.csv'
             mode = 'a' if os.path.exists(file) else 'w'
             with open(file, mode, encoding='utf-8', newline="") as f:
                 if mode == 'w':
@@ -134,7 +134,7 @@ class RollingTrainTest:
             metrics = risk_metrics.calculate_metrics(period_returns)
             
             # 保存到CSV
-            file_path = '../CSV_final/profit.csv'
+            file_path = '../CSV_final/importance/profit.csv'
             mode = 'a' if os.path.exists(file_path) else 'w'      
             with open(file_path, mode) as f:
                 if mode == 'w':
@@ -180,7 +180,7 @@ class RollingTrainTest:
                 'cum_returns': cum_returns}
             )
             os.makedirs('CSV_final', exist_ok=True)
-            file = f'../CSV_final/profit/profit_details/{self.model_name}.csv'
+            file = f'../CSV_final/importance/profit_details/{self.model_name}.csv'
             mode = 'a' if os.path.exists(file) else 'w'
             with open(file, mode, encoding='utf-8', newline="") as f:
                 if mode == 'w':
@@ -201,7 +201,7 @@ class RollingTrainTest:
             
             # 保存到CSV
             os.makedirs('CSV_final', exist_ok=True)
-            file_path = '../CSV_final/profit.csv'
+            file_path = '../CSV_final/importance/profit.csv'
             mode = 'a' if os.path.exists(file_path) else 'w'      
             with open(file_path, mode) as f:
                 if mode == 'w':
